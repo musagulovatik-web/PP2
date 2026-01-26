d56894cef4f02a1c0c3ebab8eb263892c5cb6fa0
Nurzhannat <musagulovatik@gmail.com>
2026-01-26 16:26:49 +0500

    waiting

---
+++ Practise/pyrhon-basics/Variables.py
@@ -0,0 +1,11 @@
+x=5 #Simple variable(python took it like a string)
+y="Atik" #or we can use 'Atik'
+print(x)
+print(y)
+x=float(3) #it will give us a "3.0" instead of "3" and it will change our "x" variable
+print(type(x)) #to know a type of our variable
+x, y, z ="I", "am", "writing"
+print(x,y,z)
+def myfunc(): #Function
+    print("He is"+" "+z)
+myfunc() 
---
+++ Practise/pyrhon-basics/syntax.py
@@ -0,0 +1,4 @@
+x=5
+y=3
+if x>y:
+    print("X is greater than Y")
