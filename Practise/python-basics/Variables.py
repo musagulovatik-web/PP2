7527dc9b0c2f8935a75e2f247b84ea9ae2def1da
Nurzhannat <musagulovatik@gmail.com>
2026-01-27 14:55:31 +0500

    waiting

---
+++ Practise/python-basics/Variables.py
@@ -0,0 +1,22 @@
+#1
+x=5 #Simple variable(python took it like a string)
+y="python" #or we can use 'python'
+print(x)
+print(y)
+
+#2
+x=float(3) #it will give us a "3.0" instead of "3" and it will change our "x" variable
+print(type(x)) #to know a type of our variable
+
+#3
+x, y, z ="I", "am", "writing" #multi variable
+print(x,y,z)
+
+#4
+def myfunc(): #Function
+    print("He is"+" "+z)
+myfunc() 
+
+#5
+_vas=str(23) #we can use "_" as a first symbol of our variable
+print(_vas)
