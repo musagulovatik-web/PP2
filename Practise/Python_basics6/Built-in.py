329a3411a5925b102a1d01992a27d9330baa548c
Nurzhannat <musagulovatik@gmail.com>
2026-03-17 15:08:29 +0500

    Python

---
+++ Practise/Python_basics6/Built-in.py
@@ -0,0 +1,14 @@
+import functools
+x=list(map(int, input().split()))
+y=list(map(lambda z:z*z, x))
+for u in y:
+    print(u, end=" ")
+t=list(filter(lambda z: z>21, y))
+print(t)
+g=zip(x,y)
+d=dict(g)
+for m,n in enumerate(d):
+    print(f"The square of {m} is {n}")
+sm=functools.reduce(lambda x,y: x+y, x) #sum of x
+mx=functools.reduce(lambda x,y: x if x>y else y, y)
+print(sm, mx)
